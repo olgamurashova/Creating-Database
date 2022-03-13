a94ade339dab999c1fbcf88da2560fb3c6fecc78
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-03-13 11:16:12 -0400

    Update triggers&functions.sql

--- Database Triggers/triggers&functions.sql
+++ Database Triggers/triggers&functions.sql
@@ -33,3 +33,30 @@ AFTER UPDATE ON customers
 FOR EACH STATEMENT 
 EXECUTE PROCEDURE statement_function();
 
+5. Sample / filter when the trigger calls
+
+CREATE TRIGGER insert_trigger_high
+BEFORE INSERT ON clients
+FOR EACH ROW
+WHEN (NEW.total_spent >= 1000)
+EXECUTE PROCEDURE high_spender();
+
+CREATE TRIGGER insert_trigger_low
+BEFORE INSERT ON clients
+FOR EACH ROW
+WHEN (NEW.total_spent < 1000)
+EXECUTE PROCEDURE not_a_high_spender();
+
+6.  Sample / filter when the trigger calls
+
+CREATE TRIGGER update_trigger_high
+BEFORE UPDATE ON clients
+FOR EACH ROW
+WHEN (NEW.total_spent >= 1000)
+EXECUTE PROCEDURE set_high_spender();
+
+CREATE TRIGGER update_trigger_low
+BEFORE UPDATE ON clients
+FOR EACH ROW
+WHEN (NEW.total_spent < 1000)
+EXECUTE PROCEDURE  set_low_spender();
