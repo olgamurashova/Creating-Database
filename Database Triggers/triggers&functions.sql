89b13f59e212a86dacf8284a7de15ed7c7643e09
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-03-13 03:01:13 -0400

    Update triggers&functions.sql

--- Database Triggers/triggers&functions.sql
+++ Database Triggers/triggers&functions.sql
@@ -4,3 +4,12 @@ CREATE TRIGGER check_update
     BEFORE UPDATE ON accounts
     FOR EACH ROW
     EXECUTE PROCEDURE check_account_update();
+    
+2. Sample trigger query(INSERT):
+
+CREATE TRIGGER insert_trigger
+    BEFORE INSERT ON customers
+    FOR EACH ROW
+    EXECUTE PROCEDURE insert_function();
+
+
