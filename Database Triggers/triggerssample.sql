b610765cf573cbf32c987191bdd64a49983743a9
Olga Murashova-Dreyfus <89424060+olgamurashova@users.noreply.github.com>
2022-03-14 05:50:04 -0400

    Update triggerssample.sql

--- Database Triggers/triggerssample.sql
+++ Database Triggers/triggerssample.sql
@@ -43,3 +43,28 @@ ORDER BY customer_id;
 
 SELECT *
 FROM customers_log;
+
+//Creating INSERT trigger to return a  single record of who did bulk inserts
+
+CREATE TRIGGER insert_trigger
+BEFORE INSERT ON customers
+FOR EACH STATEMENT
+EXECUTE PROCEDURE log_customers_change();
+
+// log_customers_change() function(UPDATE/INSERT)
+
+CREATE OR REPLACE FUNCTION log_customers_change() RETURNS TRIGGER AS $$
+    BEGIN
+        IF (TG_OP = 'UPDATE') THEN
+            IF (NEW.first_name <> OLD.first_name OR NEW.last_name <> OLD.last_name) THEN
+                INSERT INTO customers_log (changed_by, time_changed, change_type) VALUES (User, DATE_TRUNC('minute',NOW()), 'UPDATE');
+            END IF;
+        END IF;
+        IF (TG_OP = 'INSERT') THEN
+            INSERT INTO customers_log (changed_by, time_changed, change_type) VALUES (User, DATE_TRUNC('minute',NOW()), 'INSERT');
+        END IF;
+        RETURN NEW;
+    END;
+$$ LANGUAGE PLPGSQL;
+
+// 
